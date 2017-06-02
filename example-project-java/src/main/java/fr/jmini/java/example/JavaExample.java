599b6934132b41b4ccc0ccbf476e1f3397100934
Jeremie Bresson <dev@jmini.fr>
2017-06-02 21:49:12 +0200

    Add "example-project-java" project

---
+++ example-project-java/src/main/java/fr/jmini/java/example/JavaExample.java
@@ -0,0 +1,10 @@
+package fr.jmini.java.example;
+
+public class JavaExample {
+
+  static final String HELLO_WORLD = "Hello World";
+
+  public static void main(String[] args) throws Exception {
+    System.out.println(HELLO_WORLD);
+  }
+}
