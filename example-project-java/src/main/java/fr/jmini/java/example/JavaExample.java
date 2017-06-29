272ab3c6b4d765b86db293b2491c0694c19a2af2
Jeremie Bresson <dev@jmini.fr>
2017-06-29 21:11:39 +0200

    Add "org.slf4j" wrong usage in the example

--- example-project-java/src/main/java/fr/jmini/java/example/JavaExample.java
+++ example-project-java/src/main/java/fr/jmini/java/example/JavaExample.java
@@ -1,10 +1,15 @@
 package fr.jmini.java.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JavaExample {
+  private static final Logger LOG = LoggerFactory.getLogger(JavaExample.class);
 
   static final String HELLO_WORLD = "Hello World";
 
   public static void main(String[] args) throws Exception {
     System.out.println(HELLO_WORLD);
+    LOG.warn(String.format("Value %s", HELLO_WORLD));
   }
 }
