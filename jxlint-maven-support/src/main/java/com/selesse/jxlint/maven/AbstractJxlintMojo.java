695d350ab1a71f98ef184f42586206433be84c9f
Jeremie Bresson <dev@jmini.fr>
2017-06-29 19:03:16 +0200

    jxlint-maven-support: add support for rules and categories
    new configurations:
    
    * disableRules
    * enableOnlyRules
    * enableRules
    * enableCategories

--- jxlint-maven-support/src/main/java/com/selesse/jxlint/maven/AbstractJxlintMojo.java
+++ jxlint-maven-support/src/main/java/com/selesse/jxlint/maven/AbstractJxlintMojo.java
@@ -1,29 +1,61 @@
 package com.selesse.jxlint.maven;
 
 import java.io.File;
+import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import com.selesse.jxlint.Dispatcher;
 import com.selesse.jxlint.model.JxlintOption;
 import com.selesse.jxlint.model.ProgramOptions;
 import com.selesse.jxlint.model.rules.Categories;
 import com.selesse.jxlint.model.rules.LintRules;
 import com.selesse.jxlint.model.rules.LintRulesImpl;
+import com.selesse.jxlint.model.rules.NonExistentLintRuleException;
 import com.selesse.jxlint.settings.Profiler;
 import com.selesse.jxlint.settings.ProgramSettings;
 
 public abstract class AbstractJxlintMojo extends AbstractMojo {
 
+  private static final String DISABLE_RULES = "disableRules";
+  private static final String ENABLE_ONLY_RULES = "enableOnlyRules";
+  private static final String ENABLE_RULES = "enableRules";
+  private static final String ENABLE_CATEGORY = "enableCategories";
   private static final String SOURCE_DIRECTORY = "sourceDirectory";
   private static final String NO_WARNINGS = "noWarnings";
   private static final String ALL_WARNINGS = "allWarnings";
   private static final String WARNINGS_ARE_ERRORS = "warningAsErrors";
   private static final String OUTPUT_TYPE = "outputType";
   private static final String OUTPUT_FILE = "outputFile";
+
+  /**
+   * Disable the list of rules.
+   */
+  @Parameter(property = DISABLE_RULES)
+  protected List<String> disableRules;
+
+  /**
+   * Only check for these rules.
+   */
+  @Parameter(property = ENABLE_ONLY_RULES)
+  protected List<String> enableOnlyRules;
+
+  /**
+   * Enable the list of rules.
+   */
+  @Parameter(property = ENABLE_RULES)
+  protected List<String> enableRules;
+
+  /**
+   * Run all rules of a certain category.
+   */
+  @Parameter(property = ENABLE_CATEGORY)
+  protected List<String> enableCategories;
 
   @Parameter(property = SOURCE_DIRECTORY, defaultValue = "${project.basedir}")
   protected File sourceDirectory;
@@ -64,7 +96,7 @@ public abstract class AbstractJxlintMojo extends AbstractMojo {
 
   protected abstract Class<? extends Enum<?>> provideCategories();
 
-  protected ProgramOptions createProgramOptions() {
+  protected ProgramOptions createProgramOptions() throws MojoExecutionException {
     ProgramOptions options = new ProgramOptions();
 
     getLog().debug("set source directory option to '" + sourceDirectory.getAbsolutePath() + "'");
@@ -72,6 +104,12 @@ public abstract class AbstractJxlintMojo extends AbstractMojo {
 
     addOption(options, JxlintOption.OUTPUT_TYPE, outputType);
     addOption(options, JxlintOption.OUTPUT_TYPE_PATH, outputFile.getAbsolutePath());
+
+    addRulesOption(options, JxlintOption.CHECK, enableOnlyRules);
+    addRulesOption(options, JxlintOption.ENABLE, enableRules);
+    addRulesOption(options, JxlintOption.DISABLE, disableRules);
+
+    addCategoryOption(options, enableCategories);
 
     addBooleanOption(options, JxlintOption.NO_WARNINGS, noWarnings);
     addBooleanOption(options, JxlintOption.ALL_WARNINGS, allWarnings);
@@ -85,6 +123,46 @@ public abstract class AbstractJxlintMojo extends AbstractMojo {
     options.addOption(option, value);
   }
 
+  private void addRulesOption(ProgramOptions options, JxlintOption option, List<String> list) throws MojoExecutionException {
+    if (list != null && !list.isEmpty()) {
+      for (String rule : list) {
+        try {
+          LintRulesImpl.getInstance().getLintRule(rule);
+        }
+        catch (NonExistentLintRuleException e) {
+          throw new MojoExecutionException(e.getMessage());
+        }
+      }
+      String value = Joiner.on(',').join(list);
+      addOption(options, option, value);
+    }
+    else {
+      getLog().debug("option '" + option + "' is not set (null or empty list)");
+    }
+  }
+
+  private void addCategoryOption(ProgramOptions options, List<String> rawCategoryStringList) throws MojoExecutionException {
+    if (rawCategoryStringList != null && !rawCategoryStringList.isEmpty()) {
+      Enum<?>[] categories = Categories.get().getEnumConstants();
+      List<String> categoryNames = Lists.newArrayList();
+      for (Enum<?> category : categories) {
+        categoryNames.add(category.toString());
+      }
+
+      for (String categoryString : rawCategoryStringList) {
+        if (!categoryNames.contains(categoryString)) {
+          throw new MojoExecutionException("Category \"" + categoryString + "\" does not exist. Try one of: " +
+              Joiner.on(", ").join(categories) + ".");
+        }
+      }
+      String value = Joiner.on(',').join(rawCategoryStringList);
+      addOption(options, JxlintOption.CATEGORY, value);
+    }
+    else {
+      getLog().debug("option '" + JxlintOption.CATEGORY + "' is not set (null or empty list)");
+    }
+  }
+
   private void addBooleanOption(ProgramOptions options, JxlintOption option, boolean value) {
     if (value) {
       getLog().debug("add option '" + option + "' with default value");
@@ -95,21 +173,25 @@ public abstract class AbstractJxlintMojo extends AbstractMojo {
     }
   }
 
-  @Override
-  public void execute() throws MojoExecutionException, MojoFailureException {
-    ProgramSettings programSettings = provideProgramSettings();
-    getLog().info("running '" + programSettings.getProgramName() + "' version '" + programSettings.getProgramVersion() + "'");
-
+  protected void initJxlint() {
     LintRules lintRules = provideLintRules();
     Class<? extends Enum<?>> categories = provideCategories();
-
-    ProgramOptions programOptions = createProgramOptions();
 
     Profiler.setEnabled(false);
     Categories.setCategories(categories);
 
     LintRulesImpl.setInstance(lintRules);
     LintRulesImpl.setExitAfterReporting(false);
+  }
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    initJxlint();
+
+    ProgramSettings programSettings = provideProgramSettings();
+    getLog().info("running '" + programSettings.getProgramName() + "' version '" + programSettings.getProgramVersion() + "'");
+
+    ProgramOptions programOptions = createProgramOptions();
 
     Dispatcher dispatcher = new Dispatcher(programOptions, programSettings);
     dispatcher.dispatch();
--- jxlint-maven-support/src/test/java/com/selesse/jxlint/maven/AbstractJxlintMojoTest.java
+++ jxlint-maven-support/src/test/java/com/selesse/jxlint/maven/AbstractJxlintMojoTest.java
@@ -1,9 +1,13 @@
 package com.selesse.jxlint.maven;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,10 +17,16 @@ import com.selesse.jxlint.model.OutputType;
 import com.selesse.jxlint.model.ProgramOptions;
 import com.selesse.jxlint.model.rules.LintRules;
 import com.selesse.jxlint.settings.ProgramSettings;
+import com.selesse.jxlintimpl.CustomCategories;
+import com.selesse.jxlintimpl.rules.JxlintImplRules;
+import com.selesse.jxlintimpl.rules.impl.FunctionsStartingWithTestAreTests;
+import com.selesse.jxlintimpl.settings.JxlintImplProgramSettings;
 
 public class AbstractJxlintMojoTest {
 
-  private TestJxlintMojo mojoUnderTest;
+  private static final String CATEGORY_NAME = CustomCategories.PROBABLY_ACCIDENT.toString();
+  private static final String RULE_NAME = (new FunctionsStartingWithTestAreTests()).getName();
+  private TestJxlintImplMojo mojoUnderTest;
   protected File tempDirectory;
 
   @Before
@@ -24,11 +34,16 @@ public class AbstractJxlintMojoTest {
     tempDirectory = Files.createTempDir();
     tempDirectory.deleteOnExit();
 
-    mojoUnderTest = new TestJxlintMojo();
+    mojoUnderTest = new TestJxlintImplMojo();
+    mojoUnderTest.callInitJxlint();
   }
 
   private void initValidConfiguration() {
     mojoUnderTest.setSourceDirectory(tempDirectory);
+    mojoUnderTest.setDisableRules(null);
+    mojoUnderTest.setEnableOnlyRules(null);
+    mojoUnderTest.setEnableRules(null);
+    mojoUnderTest.setEnableCategories(null);
     mojoUnderTest.setNoWarnings(false);
     mojoUnderTest.setAllWarnings(false);
     mojoUnderTest.setWaringsAreErrors(false);
@@ -42,6 +57,9 @@ public class AbstractJxlintMojoTest {
 
     ProgramOptions options = mojoUnderTest.callCreateProgramOptions();
     assertThat(options.getSourceDirectory()).isEqualTo(tempDirectory.getAbsolutePath());
+    assertThat(options.hasOption(JxlintOption.DISABLE)).isEqualTo(false);
+    assertThat(options.hasOption(JxlintOption.CHECK)).isEqualTo(false);
+    assertThat(options.hasOption(JxlintOption.ENABLE)).isEqualTo(false);
     assertThat(options.hasOption(JxlintOption.NO_WARNINGS)).isEqualTo(false);
     assertThat(options.hasOption(JxlintOption.ALL_WARNINGS)).isEqualTo(false);
     assertThat(options.hasOption(JxlintOption.WARNINGS_ARE_ERRORS)).isEqualTo(false);
@@ -55,6 +73,10 @@ public class AbstractJxlintMojoTest {
     File srcFolder = new File(tempDirectory, "src");
 
     mojoUnderTest.setSourceDirectory(srcFolder);
+    mojoUnderTest.setDisableRules(Collections.singletonList(RULE_NAME));
+    mojoUnderTest.setEnableOnlyRules(Collections.singletonList(RULE_NAME));
+    mojoUnderTest.setEnableRules(Collections.singletonList(RULE_NAME));
+    mojoUnderTest.setEnableCategories(Collections.singletonList(CATEGORY_NAME));
     mojoUnderTest.setNoWarnings(true);
     mojoUnderTest.setAllWarnings(true);
     mojoUnderTest.setWaringsAreErrors(true);
@@ -63,6 +85,10 @@ public class AbstractJxlintMojoTest {
 
     ProgramOptions options = mojoUnderTest.callCreateProgramOptions();
     assertThat(options.getSourceDirectory()).isEqualTo(srcFolder.getAbsolutePath());
+    assertThat(options.getOption(JxlintOption.DISABLE)).isEqualTo(RULE_NAME);
+    assertThat(options.getOption(JxlintOption.CHECK)).isEqualTo(RULE_NAME);
+    assertThat(options.getOption(JxlintOption.ENABLE)).isEqualTo(RULE_NAME);
+    assertThat(options.getOption(JxlintOption.CATEGORY)).isEqualTo(CATEGORY_NAME);
     assertThat(options.hasOption(JxlintOption.NO_WARNINGS)).isEqualTo(true);
     assertThat(options.hasOption(JxlintOption.ALL_WARNINGS)).isEqualTo(true);
     assertThat(options.hasOption(JxlintOption.WARNINGS_ARE_ERRORS)).isEqualTo(true);
@@ -97,25 +123,69 @@ public class AbstractJxlintMojoTest {
     assertThat(options.getOutputType()).isEqualTo(OutputType.DEFAULT);
   }
 
-  private static class TestJxlintMojo extends AbstractJxlintMojo {
+  @Test
+  public void testInvalidRuleNameType() throws Exception {
+    try {
+      initValidConfiguration();
+      mojoUnderTest.setEnableOnlyRules(Collections.singletonList("abcd"));
+
+      mojoUnderTest.callCreateProgramOptions();
+      fail("MojoExecutionException expected");
+    }
+    catch (MojoExecutionException e) {
+      assertThat(e.getMessage()).isEqualTo("Lint rule 'abcd' does not exist.");
+    }
+  }
+
+  @Test
+  public void testInvalidCategoryNameType() throws Exception {
+    try {
+      initValidConfiguration();
+      mojoUnderTest.setEnableCategories(Collections.singletonList("xyz"));
+
+      mojoUnderTest.callCreateProgramOptions();
+      fail("MojoExecutionException expected");
+    }
+    catch (MojoExecutionException e) {
+      assertThat(e.getMessage()).isEqualTo("Category \"xyz\" does not exist. Try one of: Probably An Accident.");
+    }
+  }
+
+  private static class TestJxlintImplMojo extends AbstractJxlintMojo {
 
     @Override
     protected ProgramSettings provideProgramSettings() {
-      return null;
+      return new JxlintImplProgramSettings();
     }
 
     @Override
     protected LintRules provideLintRules() {
-      return null;
+      return new JxlintImplRules();
     }
 
     @Override
     protected Class<? extends Enum<?>> provideCategories() {
-      return null;
+      return CustomCategories.class;
     }
 
     public void setSourceDirectory(File sourceDirectory) {
       this.sourceDirectory = sourceDirectory;
+    }
+
+    public void setDisableRules(List<String> disableRules) {
+      this.disableRules = disableRules;
+    }
+
+    public void setEnableOnlyRules(List<String> enableOnlyRules) {
+      this.enableOnlyRules = enableOnlyRules;
+    }
+
+    public void setEnableRules(List<String> enableRules) {
+      this.enableRules = enableRules;
+    }
+
+    public void setEnableCategories(List<String> enableCategories) {
+      this.enableCategories = enableCategories;
     }
 
     public void setNoWarnings(boolean noWarnings) {
@@ -138,7 +208,11 @@ public class AbstractJxlintMojoTest {
       this.outputFile = outputFile;
     }
 
-    public ProgramOptions callCreateProgramOptions() {
+    public void callInitJxlint() {
+      initJxlint();
+    }
+
+    public ProgramOptions callCreateProgramOptions() throws MojoExecutionException {
       return createProgramOptions();
     }
   }
