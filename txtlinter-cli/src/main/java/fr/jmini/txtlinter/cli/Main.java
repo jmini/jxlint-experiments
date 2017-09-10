6928874158e784fbb19a0820a1169e32ae7d1f74
Jeremie Bresson <dev@jmini.fr>
2017-09-10 08:08:17 +0200

    Split txtlinter project
    * txtlinter-core
    * txtlinter-cli

--- txtlinter-cli/src/main/java/fr/jmini/txtlinter/cli/Main.java
+++ txtlinter-cli/src/main/java/fr/jmini/txtlinter/cli/Main.java
@@ -1,8 +1,9 @@
-package fr.jmini.txtlinter;
+package fr.jmini.txtlinter.cli;
 
 import com.selesse.jxlint.Jxlint;
 import com.selesse.jxlint.model.rules.Categories;
 
+import fr.jmini.txtlinter.CustomCategories;
 import fr.jmini.txtlinter.rules.TxtLinterRules;
 import fr.jmini.txtlinter.settings.TxtLinterProgramSettings;
 
